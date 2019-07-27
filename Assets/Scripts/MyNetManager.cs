43a71a712865f7f87b05b30b265076b1fbb050fa
Partha Sarker <parthasarker3@gmail.com>
2019-07-27 23:33:18 +0600

    first commit: multiplayer works

---
+++ Assets/Scripts/MyNetManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class MyNetManager : NetworkManager
+{
+
+	public NetworkDiscovery discovery;
+
+	public override void OnStartHost()
+	{
+		discovery.Initialize();
+		discovery.StartAsServer();
+
+	}
+
+	public override void OnStartClient(NetworkClient client)
+	{
+		discovery.showGUI = false;
+	}
+
+	public override void OnStopClient()
+	{
+		discovery.StopBroadcast();
+		discovery.showGUI = true;
+	}
+}
